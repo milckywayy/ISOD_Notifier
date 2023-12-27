d50130e0302cdb93768b2d370b4e46f8a6339ef1
milckywayy <dmitriwear@gmail.com>
2023-12-27 21:32:05 +0100

    Implemented ISOD request intervals

--- server/isod_handler.py
+++ server/isod_handler.py
@@ -1,4 +1,5 @@
 import asyncio
+import datetime
 import logging
 import aiohttp
 
@@ -10,11 +11,29 @@ REQUESTS_INTERVAL_TIME_SECONDS = 60
 GET_CLIENTS_QUERY = '''SELECT * FROM clients'''
 UPDATE_FINGERPRINT_QUERY = '''UPDATE clients SET news_fingerprint = ? WHERE token = ?'''
 
+TIME_INTERVALS = {
+    (0, 7): 1800,       # 00:00 - 06:59 | 30 min
+    (7, 8): 600,        # 07:00 - 07:59 | 10 min
+    (8, 16): 60,        # 08:00 - 15:59 | 1 min
+    (16, 22): 90,      # 16:00 - 21:59 | 2 min
+    (22, 24): 600,      # 22:00 - 23:59 | 10 min
+}
+
+
+def get_sleep_duration():
+    hour = datetime.datetime.now().hour
+
+    for time_range, interval in TIME_INTERVALS.items():
+        if time_range[0] <= hour < time_range[1]:
+            return interval
+    return 300  # default value in case something goes wrong
+
 
 async def check_for_new_notifications(db):
     async with aiohttp.ClientSession() as session:
         while True:
-            logging.info('Checking for ISOD news')
+            interval = get_sleep_duration()
+            logging.info('Checking for ISOD news. Interval: ' + str(interval) + 'sec')
 
             clients = db.execute(GET_CLIENTS_QUERY)
             for client in clients:
@@ -32,7 +51,7 @@ async def check_for_new_notifications(db):
                     logging.error(f"Error processing client {username}: {e}")
 
             db.commit()
-            await asyncio.sleep(REQUESTS_INTERVAL_TIME_SECONDS)
+            await asyncio.sleep(interval)
 
 
 async def start_isod_handler(app):
