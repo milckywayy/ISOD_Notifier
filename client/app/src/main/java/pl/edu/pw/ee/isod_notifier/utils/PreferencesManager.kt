5e680fbdb40bd00e874b53fbb42a2ac0d93108f3
milckywayy <dmitriwear@gmail.com>
2024-05-03 14:59:55 +0200

    Added preferences manager

---
+++ client/app/src/main/java/pl/edu/pw/ee/isod_notifier/utils/PreferencesManager.kt
@@ -0,0 +1,26 @@
+package pl.edu.pw.ee.isod_notifier.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+
+object PreferencesManager {
+    private fun getSharedPreferences(context: Context): SharedPreferences {
+        return context.getSharedPreferences("app_preferences", Context.MODE_PRIVATE)
+    }
+
+    fun saveBoolean(context: Context, key: String, value: Boolean) {
+        getSharedPreferences(context).edit().putBoolean(key, value).apply()
+    }
+
+    fun getBoolean(context: Context, key: String, defaultValue: Boolean = false): Boolean {
+        return getSharedPreferences(context).getBoolean(key, defaultValue)
+    }
+
+    fun saveString(context: Context, key: String, value: String) {
+        getSharedPreferences(context).edit().putString(key, value).apply()
+    }
+
+    fun getString(context: Context, key: String, defaultValue: String = ""): String {
+        return getSharedPreferences(context).getString(key, defaultValue) ?: defaultValue
+    }
+}
