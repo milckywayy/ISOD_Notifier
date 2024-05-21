1e119d9c200daa120d55ae71c897b26c170f775c
milckywayy <dmitriwear@gmail.com>
2024-05-21 18:04:13 +0200

    Classes server sync

--- client/app/src/main/java/pl/edu/pw/ee/isod_notifier/ui/screens/activities/ClassesScreen.kt
+++ client/app/src/main/java/pl/edu/pw/ee/isod_notifier/ui/screens/activities/ClassesScreen.kt
@@ -1,5 +1,6 @@
 package pl.edu.pw.ee.isod_notifier.ui.screens.activities
 
+import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -9,27 +10,81 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
-import kotlinx.coroutines.delay
+import com.google.firebase.logger.Logger
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import pl.edu.pw.ee.isod_notifier.http.getOkHttpClient
+import pl.edu.pw.ee.isod_notifier.http.sendRequest
 import pl.edu.pw.ee.isod_notifier.model.ClassItem
-import pl.edu.pw.ee.isod_notifier.model.NewsItem
 import pl.edu.pw.ee.isod_notifier.ui.UiConstants
 import pl.edu.pw.ee.isod_notifier.ui.common.ClassTile
 import pl.edu.pw.ee.isod_notifier.ui.common.LoadingAnimation
 import pl.edu.pw.ee.isod_notifier.ui.common.SectionText
 import pl.edu.pw.ee.isod_notifier.ui.common.TopBarScreen
+import pl.edu.pw.ee.isod_notifier.utils.PreferencesManager
+import pl.edu.pw.ee.isod_notifier.utils.extractFieldFromResponse
+import pl.edu.pw.ee.isod_notifier.utils.showToast
+import java.util.*
 
 @Composable
 fun ClassesScreen(navController: NavController) {
-    val scrollState = rememberScrollState()
+    val context = LocalContext.current
+    val httpClient = getOkHttpClient(context)
+    val scope = rememberCoroutineScope()
 
-    val terms = listOf(
-        "2022Z",
-        "2023L",
-        "2023Z",
-        "2024L"
-    ).reversed()
+    val scrollState = rememberScrollState()
+    var isLoading by remember { mutableStateOf(false) }
+
+    val terms = remember { mutableStateListOf<String>() }
+
+    LaunchedEffect(Unit) {
+        isLoading = true
+
+        sendRequest(
+            context,
+            httpClient,
+            "get_semesters",
+            mapOf(
+                "user_token" to PreferencesManager.getString(context, "USER_ID"),
+                "language" to Locale.getDefault().language
+            ),
+            onSuccess = { response ->
+                val responseBodyString = response.body?.string()
+
+                if (responseBodyString != null) {
+                    val array = responseBodyString.trim('[', ']').split(", ").map { it.trim('\"') }.toTypedArray()
+
+                    array.forEach { Log.d("Response", it) }
+
+                    terms.clear()
+                    terms.addAll(array)
+                }
+
+                isLoading = false
+            },
+            onError = { response ->
+                val responseBodyString = response.body?.string()
+
+                val message = extractFieldFromResponse(responseBodyString, "message")
+                context.showToast(message ?: "Error")
+
+                PreferencesManager.saveBoolean(context, "STATUS_CHECKED", true)
+                isLoading = false
+            },
+            onFailure = { _ ->
+                scope.launch {
+                    navController.navigate("connection_error")
+                }
+
+                PreferencesManager.saveBoolean(context, "STATUS_CHECKED", true)
+                isLoading = false
+            }
+        )
+    }
 
     TopBarScreen(
         navController,
@@ -38,40 +93,30 @@ fun ClassesScreen(navController: NavController) {
         Column(
             modifier = Modifier.verticalScroll(scrollState)
         ) {
-            ScreenContent(
-                navController,
-                terms,
-                innerPadding
-            )
+            if (terms.isNotEmpty()) {
+                ScreenContent(
+                    navController,
+                    terms,
+                    innerPadding,
+                    httpClient
+                )
+            }
         }
     }
-}
-
-private fun getClasses(term: String): List<ClassItem> {
-    if (term == "2024L") {
-        return listOf(
-            ClassItem("Sieci komputerowee wew few gew gwe jbjkbjkhvh uu", "WYK", "1"),
-            ClassItem("Toria obwodów i sygnałów", "LAB", "1"),
-            ClassItem("Metody numeryczne", "WYK","1"),
-            ClassItem("Inżynieria oprogramowania", "WYK", "1"),
-            ClassItem("Języki i metody programowania", "PRO", "1"),
-        )
-    }
-    else if (term == "2023Z") {
-        return listOf(
-            ClassItem("Toria obwodów i sygnałów", "LAB", "1"),
-            ClassItem("Języki i metody programowania", "PRO", "1"),
-        )
-    }
-    return emptyList()
 }
 
 @Composable
 fun ScreenContent(
     navController: NavController,
     terms: List<String>,
-    innerPadding: PaddingValues
+    innerPadding: PaddingValues,
+    httpClient: OkHttpClient
 ) {
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+
+    val gson = Gson()
+
     var isLoading by remember { mutableStateOf(false) }
     var dropDownMenuExpanded by remember { mutableStateOf(false) }
     var termFilter by remember { mutableStateOf(terms[0]) }
@@ -79,12 +124,57 @@ fun ScreenContent(
 
     LaunchedEffect(termFilter) {
         isLoading = true
-        delay(1000L)
 
-        classes.clear()
-        classes.addAll(getClasses(termFilter))
+        sendRequest(
+            context,
+            httpClient,
+            "get_student_courses",
+            mapOf(
+                "user_token" to PreferencesManager.getString(context, "USER_ID"),
+                "semester" to termFilter,
+                "language" to Locale.getDefault().language
+            ),
+            onSuccess = { response ->
+                val responseBodyString = response.body?.string()
 
-        isLoading = false
+                if (responseBodyString != null) {
+                    val mapType = object : TypeToken<Map<String, Any>>() {}.type
+                    val courseMap: Map<String, Any> = gson.fromJson(responseBodyString, mapType)
+                    val courses = courseMap["courses"] as List<Map<String, Any>>
+
+                    classes.clear()
+                    courses.forEach { course ->
+                        val courseName = course["name"] as String
+                        val courseId = course["course_id"] as String
+                        val classesTemp = course["classes"] as List<Map<String, String>>
+
+                        classesTemp.forEach { classInfo ->
+                            val classType = classInfo["classtype"] as String
+                            classes.add(ClassItem(courseName, classType, courseId))
+                        }
+                    }
+                }
+
+                isLoading = false
+            },
+            onError = { response ->
+                val responseBodyString = response.body?.string()
+
+                val message = extractFieldFromResponse(responseBodyString, "message")
+                context.showToast(message ?: "Error")
+
+                PreferencesManager.saveBoolean(context, "STATUS_CHECKED", true)
+                isLoading = false
+            },
+            onFailure = { _ ->
+                scope.launch {
+                    navController.navigate("connection_error")
+                }
+
+                PreferencesManager.saveBoolean(context, "STATUS_CHECKED", true)
+                isLoading = false
+            }
+        )
     }
 
     Column(
