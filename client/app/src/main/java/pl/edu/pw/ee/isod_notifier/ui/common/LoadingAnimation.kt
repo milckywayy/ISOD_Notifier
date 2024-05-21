4ce3667946584d8c904af41a3e33a85e779a1d1f
milckywayy <dmitriwear@gmail.com>
2024-05-21 17:16:08 +0200

    Adjusted loading animation

--- client/app/src/main/java/pl/edu/pw/ee/isod_notifier/ui/common/LoadingAnimation.kt
+++ client/app/src/main/java/pl/edu/pw/ee/isod_notifier/ui/common/LoadingAnimation.kt
@@ -1,29 +1,42 @@
 package pl.edu.pw.ee.isod_notifier.ui.common
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun LoadingAnimation(
     modifier: Modifier = Modifier,
-    size: Int = 60,
-    color: androidx.compose.ui.graphics.Color = MaterialTheme.colorScheme.onBackground
+    size: Int = 45,
+    strokeWidth: Dp = 4.dp,
+    indicatorColor: Color = MaterialTheme.colorScheme.onBackground,
+    backgroundColor: Color = MaterialTheme.colorScheme.surface
 ) {
     Box(
         contentAlignment = Alignment.Center,
         modifier = modifier.fillMaxSize()
     ) {
-        CircularProgressIndicator(
-            color = color,
-            strokeWidth = 5.dp,
-            modifier = Modifier.size(size.dp)
-        )
+        Box(
+            modifier = Modifier
+                .size(size.dp * 1.5f)
+                .background(color = backgroundColor, shape = CircleShape),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator(
+                color = indicatorColor,
+                strokeWidth = strokeWidth,
+                modifier = Modifier.size(size.dp)
+            )
+        }
     }
 }
